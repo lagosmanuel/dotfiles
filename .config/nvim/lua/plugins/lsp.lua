31628b503e9c768a7bd28079836564965a48ef0e
lagosmanuel <lagosmanual@gmail.com>
2025-08-15 18:51:34 -0300

    fix mason nvim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,20 +14,3 @@ local on_attach = function(_, _)
     vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
-
--- Automatically set up LSP servers installed via mason.nvim.
-require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {
-            on_attach = on_attach
-        }
-    end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    -- For example, a handler override for the `rust_analyzer`:
-    ["rust_analyzer"] = function ()
-        require("rust-tools").setup {}
-    end,
-}
